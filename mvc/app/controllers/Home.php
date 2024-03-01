ac15e518c7711d03aa2148c34dd469fa5e632c7c
eman <emangalima102602@gmail.com>
2024-03-01 15:55:26 +0800

    404 added

--- mvc/app/controllers/Home.php
+++ mvc/app/controllers/Home.php
@@ -1,14 +1,9 @@
 <?php
 
-class Home
+class Home extends Controller
 {
     public function index()
     {
-        echo 'Home/index';
+       $this->view('home');
     }
-    public function test()
-    {
-        echo 'Test';
-    }
-    
 }

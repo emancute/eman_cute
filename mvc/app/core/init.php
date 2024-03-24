92495025fc027e2d50a86b3a8ad6497e90281e67
Deniel <gaudieldeniel0@gmail.com>
2024-03-24 15:02:23 +0800

    updated

---
+++ mvc/app/core/init.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'functions.php';
+require 'Databse.php';
+require 'Controller.php';
+require 'Model.php';
+require 'App.php';
+
+spl_autoload_register(function($class_name){
+    
+    require '../app/models/' . $class_name . '.php';
+//tt
+});
+
+
+?>

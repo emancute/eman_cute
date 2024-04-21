89abadd25434b7986c352c21714772f829dd18f7
Deniel <gaudieldeniel0@gmail.com>
2024-04-21 22:52:02 +0800

    list of task activity/assignment added

---
+++ list-of-tasks/app/views/tasks/delete.php
@@ -0,0 +1,31 @@
+<?php include PATH . "partials/header.php" ?>
+
+<div class="container">
+
+  <form action="" method="POST" class="mt-5 w-50 mx-auto">
+    <h2>Delete User</h2>
+    
+        <div class="mb-2">
+            <label for="">Task Name</label>
+            <input name="task_name" value="<?= $task->task_name ?>" type="text" class="form-control">
+        </div>
+        <div class="mb-2">
+            <label for="">Task Description</label>
+            <input name="task_description" value="<?= $task->task_description ?>" type="text" class="form-control">
+        </div>
+        <div class="mb-2">
+            <label for="">Task Status</label>
+            <input name="task_status" value="<?= $task->task_status ?>" type="text" class="form-control">
+        </div>
+        <div class="mb-2">
+            <label for="">Task Due</label>
+            <input name="task_due" value="<?= $task->task_due ?>" type="datetime-local" class="form-control">
+        </div>
+        
+    <input type="hidden" name="id" value="<?= $task->id ?>">
+    <button type="submit" class="btn btn-danger">Delete</button>
+  </form>
+
+</div>
+
+<?php include PATH . "partials/footer.php" ?>
